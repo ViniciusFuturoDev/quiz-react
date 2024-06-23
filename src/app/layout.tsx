5e707d2db4a63684dd173d1776700fefd01b1885
Vinicius <136001757+ViniciusFuturoDev@users.noreply.github.com>
2024-06-23 18:02:42 -0300

    Update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Quiz</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
